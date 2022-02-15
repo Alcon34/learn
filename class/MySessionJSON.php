d7c91cf7522a8c54cb3d041c5f55384460368c80
AlconBLR <timaskov.den@gmail.com>
2022-02-15 00:17:00 -0800

    15.02.2021

--- class/MySessionJSON.php
+++ class/MySessionJSON.php
@@ -2,39 +2,69 @@
 
 class MySessionJSON implements SessionHandlerInterface
 {
-    public function open($path, $name):bool
+    /**
+     * @param $path
+     * @param $name
+     * @return bool
+     */
+    public function open($path, $name): bool
     {
         return true;
     }
+
+    /**
+     * @param $id
+     * @param $data
+     * @return bool
+     */
     public function write($id, $data): bool
     {
-        !(file_put_contents("./Session/sess_$id.json", json_encode($data,JSON_FORCE_OBJECT)) === false);
+        !(file_put_contents("./Session/sess_$id.json", json_encode($data, JSON_FORCE_OBJECT)) === false);
         return true;
     }
+
+    /**
+     * @param $id
+     * @return false|mixed|string
+     */
     public function read($id)
     {
-        if (file_exists("./Session/sess_$id.json")){
-            return json_decode(file_get_contents("./Session/sess_$id.json"),JSON_FORCE_OBJECT);
-        }else{
+        if (file_exists("./Session/sess_$id.json")) {
+            return json_decode(file_get_contents("./Session/sess_$id.json"), JSON_FORCE_OBJECT);
+        } else {
             $fp = fopen("./Session/sess_$id.json", "a+");
             fclose($fp);
             return '';
         }
     }
+
+    /**
+     * @return bool
+     */
     public function close(): bool
     {
         return true;
     }
+
+    /**
+     * @param $id
+     * @return bool
+     */
     public function destroy($id): bool
     {
-        if (file_exists("./Session/sess_$id.json")){
+        if (file_exists("./Session/sess_$id.json")) {
             unlink("./Session/sess_$id.json");
             return true;
-        }else{
+        } else {
             return false;
         }
 
     }
+
+    /**
+     * @param $max_lifetime
+     * @return bool
+     */
     public function gc($max_lifetime): bool
     {
         foreach (glob("./Session/sess_*") as $file) {
--- class/MySessionMySQL.php
+++ class/MySessionMySQL.php
@@ -3,57 +3,88 @@
 class MySessionMySQL implements SessionHandlerInterface
 {
     private $link;
+
+    /**
+     * @param $savePath
+     * @param $sessionName
+     * @return bool
+     */
     public function open($savePath, $sessionName): bool
     {
 
-        $link = mysqli_connect("localhost","epoxid_web","hS96xDA_wi2Gx8ST_","learn");
-        if($link){
+        $link = mysqli_connect("localhost", "epoxid_web", "hS96xDA_wi2Gx8ST_", "learn");
+        if ($link) {
             $this->link = $link;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    /**
+     * @return bool
+     */
     public function close(): bool
     {
         mysqli_close($this->link);
         return true;
     }
+
+    /**
+     * @param $id
+     * @return false|mixed|string
+     */
     public function read($id)
     {
-        $result = mysqli_query($this->link,"SELECT Session_Data FROM Session WHERE Session_Id = '".$id."' AND Session_Expires > '".date('Y-m-d H:i:s')."'");
-        if($row = mysqli_fetch_assoc($result)){
+        $result = mysqli_query($this->link, "SELECT Session_Data FROM Session WHERE Session_Id = '" . $id . "' AND Session_Expires > '" . date('Y-m-d H:i:s') . "'");
+        if ($row = mysqli_fetch_assoc($result)) {
             return $row['Session_Data'];
-         }else{
+        } else {
             return "";
         }
     }
+
+    /**
+     * @param $id
+     * @param $data
+     * @return bool
+     */
     public function write($id, $data): bool
     {
         $DateTime = date('Y-m-d H:i:s');
-        $NewDateTime = date('Y-m-d H:i:s',strtotime($DateTime.' + 1 hour'));
-        $result = mysqli_query($this->link,"REPLACE INTO Session SET Session_Id = '".$id."', Session_Expires = '".$NewDateTime."', Session_Data = '".$data."'");
-        if($result){
+        $NewDateTime = date('Y-m-d H:i:s', strtotime($DateTime . ' + 1 hour'));
+        $result = mysqli_query($this->link, "REPLACE INTO Session SET Session_Id = '" . $id . "', Session_Expires = '" . $NewDateTime . "', Session_Data = '" . $data . "'");
+        if ($result) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    /**
+     * @param $id
+     * @return bool
+     */
     public function destroy($id): bool
     {
-        $result = mysqli_query($this->link,"DELETE FROM Session WHERE Session_Id ='".$id."'");
-        if($result){
+        $result = mysqli_query($this->link, "DELETE FROM Session WHERE Session_Id ='" . $id . "'");
+        if ($result) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    /**
+     * @param $max_lifetime
+     * @return bool
+     */
     public function gc($max_lifetime): bool
     {
-        $result = mysqli_query($this->link,"DELETE FROM Session WHERE ((UNIX_TIMESTAMP(Session_Expires) + ".$max_lifetime.") < ".$max_lifetime.")");
-        if($result){
+        $result = mysqli_query($this->link, "DELETE FROM Session WHERE ((UNIX_TIMESTAMP(Session_Expires) + " . $max_lifetime . ") < " . $max_lifetime . ")");
+        if ($result) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
